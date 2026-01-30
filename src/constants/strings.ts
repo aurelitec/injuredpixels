ab1c5fde778520bb1320f08519d115976205a9a6
TechAurelian <dev@techaurelian.com>
2026-01-30 13:37:11 +0200

    refactor(strings): update toggle panel description to 'Hide/show controls'

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -45,7 +45,7 @@ export const KEYS_TOUCH_HOLD = 'Touch and hold';
 export const DESC_JUMP_TO_COLOR = 'Jump to color';
 export const DESC_CYCLE_COLORS = 'Cycle colors';
 export const DESC_FULLSCREEN = 'Fullscreen';
-export const DESC_TOGGLE_PANEL = 'Toggle panel';
+export const DESC_TOGGLE_PANEL = 'Hide/show controls';
 export const DESC_EXIT_FULLSCREEN = 'Exit fullscreen';
 export const DESC_NEXT_COLOR = 'Next color';
 
