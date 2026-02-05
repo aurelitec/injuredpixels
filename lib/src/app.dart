4e83fee4e6f57275cb7f3acd7095d9de25a2c8ce
TechAurelian <dev@techaurelian.com>
2026-02-05 13:41:21 +0200

    Add App class

---
+++ lib/src/app.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2009-2026 Aurelitec. All rights reserved.
+// https://www.aurelitec.com/injuredpixels/
+// Licensed under the MIT License.
+
+/// Main application class.
+class App {
+  /// Runs the application.
+  void run() {
+    // TODO: Implement in Phase 3
+  }
+}
